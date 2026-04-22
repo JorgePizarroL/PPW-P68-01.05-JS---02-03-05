f2ed16c4eca37dc7bf71fdf16292300ed96f19a6
JorgePizarroL <jpizarrol1@est.ups.edu.ec>
2026-04-21 21:20:09 -0500

    ADD js Dom Basico

---
+++ 01-sintaxis/JS/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const nombre = 'Jorge';
+const apellido = 'Pizarro';
+let ciclo = 5;
+const activo = true;
+
+const direccion = {
+
+    ciudad: 'Cuenca',
+    provincia: 'Azuay'
+
+}
+
+// imprimir todas las variables
+
+console.table({nombre, apellido, ciclo, activo, direccion});
+
+// const calcularPromedio = (notas) => // promedio; notas.reduce=>sum()
+
+const esMayorEdad = (edad) => edad >= 18;
+
+const getSaludo = (nombre, hora) => {
+
+    if (hora < 12)
+        return 'Buenos dias', '{nombre}';
+    if (hora < 18) 
+        return 'Buenas Tardes', '{nombre}';
+    return 'Buenas noches', '{nombre}';
+
+}
+
+const getSaludo2 = (nombre, hora) => hora < 12
+    ? `Buenos dias, ${nombre}`
+    : hora < 18
+        ? `Buenas Tardes, ${nombre}`
+        : `Buenas Noches, ${nombre}`;
+
+// Mostrar Html
+
+document.getElementById('nombre').textContent = `${nombre}`;
+document.getElementById('apellido').textContent = `${apellido}`;
+document.getElementById('ciclo').textContent = `${ciclo}`;
+
+//Crear repositorio subirlo
+
